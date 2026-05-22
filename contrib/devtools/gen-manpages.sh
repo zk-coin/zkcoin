5319db0c0d47cba693ffcc8985d2584abd93c854
RustNinja <placex.com@gmail.com>
2026-05-23 02:53:26 +0800

    build: align release identity artifacts

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -18,8 +18,10 @@ BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
-# The autodetected version git tag can screw up manpage output a little bit
-read -r -a BTCVER <<< "$($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }')"
+# The autodetected version git tag can screw up manpage output a little bit.
+# Strip every suffix after the base version so dirty local builds do not leave
+# generated manpages with transient git state.
+read -r -a BTCVER <<< "$($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7, $8 }')"
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
@@ -30,7 +32,9 @@ $BITCOIND --version | sed -n '1!p' >> footer.h2m
 for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $WALLET_TOOL $BITCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  for suffix in "${BTCVER[@]:1}"; do
+    [ -n "$suffix" ] && sed -i "s/\\\-${suffix}//g" ${MANDIR}/${cmdname}.1
+  done
 done
 
 rm -f footer.h2m
--- test/lint/lint-zkcoin-product-identity.sh
+++ test/lint/lint-zkcoin-product-identity.sh
@@ -34,10 +34,48 @@ check_contains doc/README_windows.txt "zkCoin Core" "Windows README product titl
 check_contains contrib/debian/litecoin-qt.desktop "Name=zkCoin Core" "Debian desktop display name"
 check_contains src/bitcoind-res.rc '"CompanyName",        "zkCoin"' "Windows daemon company name"
 check_contains src/qt/res/bitcoin-qt-res.rc '"CompanyName",        "zkCoin"' "Windows GUI company name"
+check_contains doc/man/litecoind.1 "Start zkCoin Core" "daemon manpage product action"
+check_contains doc/man/litecoin-cli.1 "Send command to zkCoin Core" "CLI manpage product action"
+check_contains build_msvc/bitcoin_config.h '#define CLIENT_VERSION_BUILD 5' "MSVC version build"
+check_contains build_msvc/bitcoin_config.h '#define COPYRIGHT_HOLDERS_FINAL "The zkCoin developers"' "MSVC copyright holder"
+check_contains build_msvc/bitcoin_config.h '#define COPYRIGHT_HOLDERS_SUBSTITUTION "zkCoin"' "MSVC copyright substitution"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_BUGREPORT "https://github.com/zk-coin/zkcoin/issues"' "MSVC bug-report URL"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_NAME "zkCoin Core"' "MSVC package name"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_STRING "zkCoin Core 0.21.5.5"' "MSVC package string"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_TARNAME "litecoin"' "unchanged MSVC package tarname"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_URL "https://github.com/zk-coin/zkcoin"' "MSVC package URL"
+check_contains build_msvc/bitcoin_config.h '#define PACKAGE_VERSION "0.21.5.5"' "MSVC package version"
+check_contains build_msvc/README.md "Building zkCoin Core with Visual Studio" "MSVC README title"
+check_contains build_msvc/bitcoind/bitcoind.vcxproj 'Replace="@PACKAGE_NAME@" By="zkCoin Core"' "MSVC test package name"
+check_contains contrib/devtools/gen-manpages.sh 'for suffix in "${BTCVER[@]:1}"' "manpage version suffix cleanup"
 check_contains configure.ac "BITCOIN_DAEMON_NAME=litecoind" "unchanged daemon binary name"
 check_contains configure.ac "BITCOIN_CLI_NAME=litecoin-cli" "unchanged CLI binary name"
 check_contains configure.ac "BITCOIN_GUI_NAME=litecoin-qt" "unchanged GUI binary name"
 check_contains src/util/system.cpp 'BITCOIN_CONF_FILENAME = "litecoin.conf"' "unchanged config filename"
 check_contains src/util/system.cpp 'pathRet / ".litecoin"' "unchanged Unix datadir"
+
+if grep -R -Fq "Litecoin Core" doc/man; then
+  echo "doc/man: stale Litecoin Core identity remains"
+  grep -R -Fn "Litecoin Core" doc/man
+  EXIT_CODE=1
+fi
+
+if grep -R -Fq "https://litecoin.org/" doc/man; then
+  echo "doc/man: stale litecoin.org URL remains"
+  grep -R -Fn "https://litecoin.org/" doc/man
+  EXIT_CODE=1
+fi
+
+if grep -R -Fq "https://github.com/litecoin-project/litecoin" doc/man; then
+  echo "doc/man: stale upstream source URL remains"
+  grep -R -Fn "https://github.com/litecoin-project/litecoin" doc/man
+  EXIT_CODE=1
+fi
+
+if grep -R -Fq "dirty" doc/man; then
+  echo "doc/man: dirty generated version suffix remains"
+  grep -R -Fn "dirty" doc/man
+  EXIT_CODE=1
+fi
 
 exit ${EXIT_CODE}
